61828cd25ed9498cc0f42c358d9e7993d2f5d69f
Carlos Erick <73728755+Carlos0101py@users.noreply.github.com>
2024-05-21 00:51:41 -0300

    feat: melhorias no script de interação com o DOM, podendo acrecentar estilos de seleção

---
+++ app/static/js/interactionDOM.js
@@ -0,0 +1,94 @@
+
+function hiddenNotification(element) {
+    element.style.display = "none"
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+    notification = document.querySelector('.notification_div')
+
+    if (notification != null) {
+        setTimeout(() => hiddenNotification(notification), 5000)
+    }
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    body = document.getElementById('anexoBody')
+    inputFile = document.getElementById('anexo')
+    parquivo = document.getElementById('NomeArquivo')
+
+    body.addEventListener('click', () => {
+
+        inputFile.click()
+
+        inputFile.addEventListener('change', () => {
+
+            const curFile = inputFile.files
+
+            body.style.backgroundColor = '#14532D'
+
+            for (file of curFile) {
+                parquivo.textContent = file.name
+            }
+        })
+    })
+})
+
+
+
+function divCheckOneStyle(element) {
+    if (element.classList.contains('active')) {
+        element.classList.remove('active')
+        element.style.backgroundColor = ''
+
+        description = element.getElementsByTagName('span')[0]
+        description.style.color = ('rgb(107, 114, 128, 1)')
+    } else {
+        element.classList.add('active')
+        element.style.backgroundColor = ('#14532D')
+
+        description = element.getElementsByTagName('span')[0]
+        description.style.color = ('rgb(255, 255, 255, 1)')
+    }
+};
+
+function toggleCheckOne(bodyDiv, clicked) {
+
+    for (element of bodyDiv) {
+
+        if (element.id) {
+            if (element === clicked) {
+                checkbox = element.querySelector('.checked')
+                checkbox.checked = !checkbox.checked
+            } else {
+                checkbox = element.querySelector('.checked')
+                element.classList.add('active')
+                checkbox.checked = false;
+            }
+
+            divCheckOneStyle(element)
+        }
+    }
+};
+
+function toggleCheck(element) {
+
+    checkbox = element.querySelector('.checked')
+    checkbox.checked = !checkbox.checked
+    divCheckOneStyle(element)
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    bodyDiv = document.querySelectorAll('.bodyDiv')
+
+    bodyDiv.forEach(element => {
+        element.addEventListener('click', () => {
+
+            if (element.id === "products") {
+                toggleCheckOne(bodyDiv, element)
+            } else {
+                toggleCheck(element)
+            }
+        });
+    });
+}); 
